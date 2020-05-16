0bfed89242a7171a51a3816e8b1931caec4c3a46
CasaRol <32219634+CasaRol@users.noreply.github.com>
2020-05-16 12:08:40 +0200

    gitignore doesn't exclude build

---
+++ React/music-app/build/precache-manifest.ab9e8e40ccfafd3d1dba4f97679f0c18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa00f540a8acc413b8e5c6d786de13a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7e0d81d7938ca7a270",
+    "url": "/static/css/main.44d5f596.chunk.css"
+  },
+  {
+    "revision": "5aa32b3dde433e150a0d",
+    "url": "/static/js/2.4f46f798.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4f46f798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7e0d81d7938ca7a270",
+    "url": "/static/js/main.2586cc9e.chunk.js"
+  },
+  {
+    "revision": "7578e853f786ff058189",
+    "url": "/static/js/runtime-main.bd695dc2.js"
+  },
+  {
+    "revision": "0d2bf90c37a001ef7bfcbf5612aca52d",
+    "url": "/static/media/placeholder_logo.0d2bf90c.jpg"
+  }
+]);
