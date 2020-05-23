69fe2d472e512e18b52ba485597c980f965f3b9b
CasaRol <alexander-rol@hotmail.com>
2020-05-23 21:27:10 +0200

    Editet Spelling errors in comments to take screen shots for the report

--- React/music-app/src/env_vars.js
+++ React/music-app/src/env_vars.js
@@ -1,16 +1,16 @@
-//for when runing servers locally
+//For when runing servers locally
 //export const LOGIN_SERVER = 'http://localhost:8080';
 //export const MUSIC_SERVER = 'http://localhost:2040';
 //export const IMAGE_SERVER = 'http://localhost:2020';
 //export const METADATA_SERVER = 'http://localhost:2060';
 
-//for useing docker compose containers
+//For using docker compose containers
 //  export const LOGIN_SERVER = 'https://192.168.99.100:443/login/';
 //  export const MUSIC_SERVER = 'https://192.168.99.100:443/music/';
 //  export const IMAGE_SERVER = 'https://192.168.99.100:443/image/';
 //  export const METADATA_SERVER = 'https://192.168.99.100:443/metadata/';
 
-//for useing docker compose containers
+//For using GKE containers
  export const LOGIN_SERVER = 'https://35.244.219.97:443/login';
  export const MUSIC_SERVER = 'https://35.244.219.97:443/music';
  export const IMAGE_SERVER = 'https://35.244.219.97:443/image';
